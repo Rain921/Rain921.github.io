7f7c4c60b9fd7c2743f689576448eea44e29b5a6
Rain921 <56>
2018-06-26 04:26:19 +0300

    test-
    test

---
+++ enigma/index.php
@@ -0,0 +1,274 @@
+<?php include 'header.php'; ?>
+<section class="product wrapper">
+	<h2>Производим коллекции платьев с дизайном тканей
+	от мировых брендов Gucci, Dior, D&G</h2>
+	<div class="description">
+		<div class="description__img">
+			<img src="dist/img/img.jpg" alt="img">
+		</div>
+		<div class="description__text" style="background: url(dist/img/border.jpg) no-repeat center;" >
+			<span>18 лет опыта изготовления платьев</span>
+			<p>Мы изготовили платья для более чем 12 000 оптовых клиентов по всей Украине. Этот опыт позволил нам создать качественную продукцию и подготовить выгодные условия сотрудничества для наших клиентов.</p>
+			<div class="women">
+				<img src="dist/img/women-4.png" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="description">
+		<div class="description__imgL">
+			<img src="dist/img/img-2.jpg" alt="img">
+		</div>
+		<div class="description__text description__text_left" style="background: url(dist/img/border.jpg) no-repeat center;" >
+			<span>4 раза в год создаем новые коллекции</span>
+			<p>Под каждый сезон мы выпускаем новую коллекцию — выпускных, свадебных, деловых и коктейльных платьев. Поэтому Вы будете иметь самый эксклюзивный и продаваемый товар по сезону. Вам не нужно трать время на поиск нового ассортимента и поставщиков на следующий сезон. Более 12 000 единиц всегда в наличии.</p>
+			<div class="women__L">
+				<img src="dist/img/women-5.png" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="description">
+		<div class="description__img">
+			<img src="dist/img/img-3.jpg" alt="img">
+		</div>
+		<div class="description__text" style="background: url(dist/img/border.jpg)  no-repeat center;" >
+			<span>Используем турецкие ткани
+			для производства платьев</span>
+			<p>Мы заказываем дизайны тканей которые производятся специально для нас и больше никому на Украине не продаются. Также мы используем ткани с дизайном от мировых брендов Gucci, Dior, D&G. Для изготовления платьев используем шифон, гипюр, французское кружево, трикотаж, атлас, бархат, 3-Д гипюр, итальянский жаккард, креп – шифон.</p>
+			<div class="women">
+				<img src="dist/img/women-7.png" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="description">
+		<div class="description__imgL">
+			<img src="dist/img/img-4.jpg" alt="img">
+		</div>
+		<div class="description__text description__text_left" style="background: url(dist/img/border.jpg);" >
+			<span>Оплата и доставка</span>
+			<p>Оплата производится картой любого банка или наложенным платежом. Также работаем по оплате на р\с без НДС. Доставку осуществляем службами доставки Новая почта, Intime, Деливери, Укрпочта.</p>
+			<div class="women__L">
+				<img src="dist/img/women-6.png" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="description">
+		<div class="description__img">
+			<img src="dist/img/img-5.jpg" alt="img">
+		</div>
+		<div class="description__text" style="background: url(dist/img/border.jpg) no-repeat center;" >
+			<span>Возврат и обмен в течение 14 дней</span>
+			<p>Наши специалисты подберут для Вас самый продаваемый товар. Если Вы захотите поменять товар на другие позиции или вернуть деньги,
+			в течение 14 дней мы осуществим возврат или обмен.</p>
+			<div class="women">
+				<img src="dist/img/women-8.png" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="costume" style="background: url(dist/img/clacs.png) no-repeat -18% 15%, url(dist/img/clac2.png)no-repeat 117% bottom ">
+	<div class="wrapper">
+	<h2>Выбирайте из более 700 моделей платьев, которые всегда в наличии</h2>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav">
+			<li><img src="dist/img/women-8.2.png" alt="img"></li>
+			<li><img src="dist/img/women-1.2.png" alt="img"></li>
+			<li><img src="dist/img/women-1.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Вечерние</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav2">
+			<li><img src="dist/img/women-9.png" alt="img"></li>
+			<li><img src="dist/img/women-2.2.png" alt="img"></li>
+			<li><img src="dist/img/women-2.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Выпускные</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav2; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav3">
+			<li><img src="dist/img/women-10.png" alt="img"></li>
+			<li><img src="dist/img/women-3.2.png" alt="img"></li>
+			<li><img src="dist/img/women-3.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Коктейльные</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav3; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav4">
+			<li><img src="dist/img/women-11.png" alt="img"></li>
+			<li><img src="dist/img/women-4.2.png" alt="img"></li>
+			<li><img src="dist/img/women-4.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Бигсайз</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav4; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav5">
+			<li><img src="dist/img/women-12.png" alt="img"></li>
+			<li><img src="dist/img/women-5.2.png" alt="img"></li>
+			<li><img src="dist/img/women-5.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Трансформер</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav5; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav6">
+			<li><img src="dist/img/women-13.png" alt="img"></li>
+			<li><img src="dist/img/women-6.2.png" alt="img"></li>
+			<li><img src="dist/img/women-6.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Топ с юбкой</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav6; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav7">
+			<li><img src="dist/img/women-14.png" alt="img"></li>
+			<li><img src="dist/img/women-7.2.png" alt="img"></li>
+			<li><img src="dist/img/women-7.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Костюмы и платья <br>
+			в деловом стиле</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav7; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	<div class="costume-product" style="background: url(dist/img/women-bg.png) no-repeat right;">
+		<ul class="costume-product__img uk-switcher" id="component-nav8">
+			<li><img src="dist/img/women-15.png" alt="img"></li>
+			<li><img src="dist/img/women-8.22.png" alt="img"></li>
+			<li><img src="dist/img/women-8.3.png" alt="img"></li>
+		</ul>
+		<div class="costume-product__nav">
+			<span>Свадебные</span>
+			<ul class="uk-nav uk-nav-default" uk-switcher="connect: #component-nav8; animation: uk-animation-fade;">
+				<li><a href="#" class="product-white"></a></li>
+				<li><a href="#" class="product-red"></a></li>
+				<li><a href="#" class="product-black"></a></li>
+			</ul>
+			<button class="btn-product">Узнать стоимость</button>
+		</div>
+	</div>
+	</div>
+</section>
+<div class="callback" >
+
+	<div class="form-block" style="background-image: url(dist/img/form-fon.png);">
+		<div class="form-block__text">
+		<h4>Заполните форму, и в течение 15 мин мы пришлем Вам
+		на почту полный фотокаталог нашей продукции с ценами</h4>
+	</div>
+	<div class="form-block__form">
+		<form id="form2" class="form-order form2" name="form2">
+			<div class="form-order__input">
+				<input type="text" name="name" value="" placeholder="ваше имя">
+			</div>
+			<div class="form-order__input">
+				<input type="text" name="email" value="" placeholder="Ваш email">
+			</div>
+			<div class="form-order__input">
+				<input type="text" name="phone" value="" placeholder="ваш телефон">
+			</div>
+			<div class="form-order__btn">
+				<button href="#" class="btn js-close-thank-you">
+				<input type="submit" name="button" value="Получить фотокаталог">
+				</button>
+			</div>
+		</form>
+	</div>
+	<img class="form-absolut" src="dist/img/form-absolut.png">
+	<img class="form-absolut2" src="dist/img/form-absolut2.png">
+</div>
+</div>
+
+  <div id="modal-container" class="uk-modal-container" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body" style="background-color: #fcf4f3;">
+        <form id="form"  class="modals-form form" name="form">
+             <button class="uk-modal-close-default" type="button" uk-close></button>
+             <div class="modals-form__text">
+             <span>Заполните форму,</span>
+             <p>и наш менеджер перезвонит Вам в течение 15 мин
+				чтобы ответить на все вопросы</p>
+             </div>
+          <div class="modals-form__input-wrap">
+            <input type="text" class="modals-form__input form-control" name="name" value="" placeholder="Ваше имя">
+          </div>
+          <div class="modals-form__input-wrap">
+            <input type="text" class="modals-form__input form-control" value="" name="phone" placeholder="Ваш телефон">
+          </div>
+           <div class="modals-form__input-wrap">
+            <input type="text" class="modals-form__input form-control" name="email" value="" placeholder="Ваш email">
+          </div>
+          <div class="modals-form__btn">
+          <a href="#" class="">
+            <input type="submit" name="button" placeholder="перезвоните мне" value="перезвоните мне" class="">
+          </a>
+        </div>
+        </form>
+        </div>
+    </div>
+<!--попап благодарности-->
+	<div class="thanks-overlay">
+		<div class="thanks-popup js-thank-you">
+			<div class="thank-you" style="background: url(dist/img/thanks-popup.jpg) no-repeat;">
+				<div class="thank-you__tittle">
+					<h2>Благодарим Вас!</h2>
+					<p>Ваша заявка отправлена нашему менеджеру.
+					Он перезвонит Вам через 2 минуты.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<script src="/dist/js/app.min.js"></script>
+<script src="dist/js/uikit.min.js"></script>
+</body>
+</html>
