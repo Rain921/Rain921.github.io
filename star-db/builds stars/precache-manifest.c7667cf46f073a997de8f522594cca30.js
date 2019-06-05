38daa5bf1ad5b5568acf6c73ee645042ba2abfa5
Rain921 <56>
2019-06-05 20:09:57 +0300

    lk

---
+++ star-db/builds stars/precache-manifest.c7667cf46f073a997de8f522594cca30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11fde1afe437458b0fcc",
+    "url": "/static/js/main.11fde1af.chunk.js"
+  },
+  {
+    "revision": "6171f064f4f5586b3063",
+    "url": "/static/js/2.6171f064.chunk.js"
+  },
+  {
+    "revision": "11fde1afe437458b0fcc",
+    "url": "/static/css/main.8404a7b0.chunk.css"
+  },
+  {
+    "revision": "3e05b69b47db9437a5a410e0d1fe895d",
+    "url": "/index.html"
+  }
+];
